04f249e9fd91c0e97451a22ea5ab108ac6b46ab2
juan-rybczinski <jhreplay.lee@gmail.com>
2023-06-18 14:50:49 +0900

    SingleChildScrollView - 기본 렌더링

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,13 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:scrollable_widgets/layout/main_layout.dart';
+import 'package:scrollable_widgets/screen/single_child_scroll_view_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder(
-      child: Text(
-        'HomeScreen',
+    return MainLayout(
+      title: 'HomeScreen',
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            ElevatedButton(
+              onPressed: () => Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (_) => SingleChildScrollViewScreen(),
+                ),
+              ),
+              child: Text('SingleChildScrollView'),
+            ),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/screen/single_child_scroll_view_screen.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:scrollable_widgets/const/colors.dart';
+import 'package:scrollable_widgets/layout/main_layout.dart';
+
+class SingleChildScrollViewScreen extends StatelessWidget {
+  const SingleChildScrollViewScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MainLayout(
+      title: 'SingleChildScrollView',
+      body: renderSimple(),
+    );
+  }
+
+  SingleChildScrollView renderSimple() {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          for (final color in rainbowColors) renderContainer(color: color),
+        ],
+      ),
+    );
+  }
+
+  Widget renderContainer({
+    required Color color,
+  }) {
+    return Container(
+      height: 300,
+      color: color,
+    );
+  }
+}
