9f7b8d9f496bd3fd0add42c04d72a300cc50e05b
juan-rybczinski <jhreplay.lee@gmail.com>
2023-07-09 20:50:26 +0900

    CustomScrollView - ChildSliverList

---
+++ lib/screen/custom_scroll_view_screen.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:scrollable_widgets/const/colors.dart';
+import 'package:scrollable_widgets/const/numbers.dart';
+
+class CustomScrollViewScreen extends StatelessWidget {
+  const CustomScrollViewScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            title: Text('CustomScrollViewScreen'),
+          ),
+          renderChildSliverList(),
+        ],
+      ),
+    );
+  }
+
+  SliverList renderChildSliverList() {
+    return SliverList(
+      delegate: SliverChildListDelegate(
+        [
+          for (final number in numbers)
+            renderContainer(
+              color: rainbowColors[number % rainbowColors.length],
+              index: number,
+            ),
+        ],
+      ),
+    );
+  }
+
+  Widget renderContainer({
+    required Color color,
+    required int index,
+    double? height,
+  }) {
+    debugPrint(index.toString());
+    return Container(
+      key: Key(index.toString()),
+      height: height ?? 300,
+      color: color,
+      child: Center(
+        child: Text(
+          index.toString(),
+          style: const TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.w700,
+            fontSize: 30.0,
+          ),
+        ),
+      ),
+    );
+  }
+}
